5becd4d7a5a2841e99b304f010b24e012e1559b6
Kim Kyu San <developerstarlight@gmail.com>
2019-10-11 17:40:32 +0900

    init html view for study

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-http.createServer().listen(80, () => {
+const app = express()
+const router = express.Router()
+
+router.route('/').get((req, res) => {
+  res.writeHead(200, {'Content-type': 'text/html;charset=utf8'})
+  res.end('<h1>유승민 ㅄ</h1>')
+})
+
+app.use('/', express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+app.use(router)
+
+app.listen(80, () => {
   console.log('server on')
 })
